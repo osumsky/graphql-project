e77d16ddff8d2baf20b922eb4986101be4d650c1
osumsky <osumsky@gmail.com>
2023-02-28 17:45:05 +0200

    Server has been added

--- server/index.js
+++ server/index.js
@@ -4,15 +4,40 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
 
 const PORT = process.env.PORT || 5000;
+const users = [
+  { id: 1, username: 'Oleg', age: 46 },
+  { id: 2, username: 'Igor', age: 27 },
+  { id: 3, username: 'Elena', age: 35 },
+];
 
 const app = express();
 app.use(cors());
+
+const rootValue = {
+  getAllUsers: () => {
+    return users;
+  },
+  getUser: ({ id }) => {
+    // id comes as string
+    return users.find((user) => user.id == id);
+  },
+  createUser: ({ input }) => {
+    const id = Date.now();
+    const createdUser = {
+      id,
+      ...input,
+    };
+    users.push(createdUser);
+    return createdUser;
+  },
+};
 
 app.use(
   '/graphql',
   graphqlHTTP({
     graphiql: true,
     schema,
+    rootValue,
   })
 );
 
--- server/schema.js
+++ server/schema.js
@@ -13,6 +13,29 @@ const schema = buildSchema(`
     title: String
     content: String
   }
+
+  input UserInput {
+    id: ID
+    username: String!
+    age: Int!
+    posts: [PostInput]
+  }
+
+  input PostInput {
+    id: ID
+    title: String!
+    content: String!
+  }
+
+  type Query {
+    getAllUsers: [User]
+    getUser(id: ID): User
+  }
+
+  type Mutation {
+    createUser(input: UserInput): User
+  }
+
 `);
 
 module.exports = schema;

f493204f39bd1b247e42bff5ea67a92f099744aa
osumsky <osumsky@gmail.com>
2023-02-28 15:27:37 +0200

    Added express to server part

---
+++ server/schema.js
@@ -0,0 +1,18 @@
+const {buildSchema} = require('graphql');
+
+const schema = buildSchema(`
+  type User {
+    id: ID
+    username: String
+    age: Int
+    posts: [Post]
+  }
+
+  type Post {
+    id: ID
+    title: String
+    content: String
+  }
+`);
+
+module.exports = schema;

d524802c4cc5fcb94c7327b373cd2748e7d54210
osumsky <osumsky@gmail.com>
2023-02-28 22:27:06 +0200

    Added get users functionality (client)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_ALL_USERS, GET_ONE_USER } from './query/user';
+import { CREATE_USER } from './mutations/user';
+
+const defaultUser = {
+  username: '',
+  age: 0,
+};
 
 function App() {
+  const { data, loading, refetch } = useQuery(GET_ALL_USERS);
+
+  const [newUser] = useMutation(CREATE_USER);
+
+  const addUser = (e: any) => {
+    e.preventDefault();
+    newUser({
+      variables: { input: { username: user.username, age: Number(user.age) } },
+    })
+      .then(({ data: { createUser } }) => {
+        setUser(defaultUser);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const getAll = (e: any) => {
+    e.preventDefault();
+    refetch();
+  };
+
+  const { data: oneUser, loading: loadingOneUser } = useQuery(GET_ONE_USER, {
+    variables: { id: 1 },
+  });
+  console.log(oneUser);
+  
+
+  const [users, setUsers] = useState([]);
+
+  const [user, setUser] = useState(defaultUser);
+
+  const onInputChange = (e: any) => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  useEffect(() => {
+    if (!loading) setUsers(data.getAllUsers);
+  }, [data, loading]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input
+          name="username"
+          value={user.username}
+          onChange={onInputChange}
+          type="text"
+        />
+        <input
+          name="age"
+          value={user.age}
+          onChange={onInputChange}
+          type="number"
+        />
+        <div>
+          <button onClick={(e) => addUser(e)}>Create</button>
+          <button onClick={(e) => getAll(e)}>Get</button>
+        </div>
+      </form>
+
+      <div>
+        {users.map(({ id, username, age }) => (
+          <div key={id} className="user">
+            {id}. {username} ({age})
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
